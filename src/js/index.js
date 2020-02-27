e699c6de0c134cb14b0b41ca379b69ce2775e492
DarthJktu <DarthJktu@gmail.com>
2020-02-27 18:09:51 +0200

    Initial commit

---
+++ src/js/index.js
@@ -0,0 +1,28 @@
+import 'owl.carousel';
+
+import Blazy from 'blazy';
+
+// import '@fancyapps/fancybox';
+
+import Inputmask from "inputmask/dist/inputmask/inputmask.date.extensions";
+
+// import "jquery-ui/ui/widgets/slider";
+
+// import "@fancyapps/fancybox/dist/jquery.fancybox.min";
+
+// import "slick-slider/slick/slick.min";
+
+// import 'malihu-custom-scrollbar-plugin/jquery.mCustomScrollbar.concat.min'
+
+// Inputmask({"mask": "+7 (999) 999 - 99 - 99"}).mask('.phone');
+
+
+
+
+$(document).ready(function () {
+
+
+
+
+});
+
